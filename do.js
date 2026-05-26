d23dc98a475f3204e588c308aaa5adf563cd9526
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-05-26 08:05:28 -0500

    Update do.js

--- do.js
+++ do.js
@@ -8,3 +8,31 @@ try{globals.add(this);}catch{}
 try{globals.add(document);}catch{}
 try{globals.add(Object);}catch{}
 
+const walkProps = (obj,props) =>{
+  let res = obj;
+  for(const prop of props){
+    res = res?.[prop];
+  }
+  return res;
+};
+
+const findProp = name =>{
+  const keys = name.split('.');
+  for(const obj of globals){
+    const res = walkProps(obj,keys);
+    if(res){
+      return res;
+    }
+  }
+};
+
+function $do(commands){
+  let res;
+  for(const cmd of commands){
+    const parts = Object.entries(cmd);
+    const fn = findProp(cmd[0]);
+    const args = cmd[1];
+    res = fn(...args);
+  }
+  return res;
+}
